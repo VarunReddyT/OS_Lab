6d13762133438a5c6c297a9c30e97ef2fd5f7197
Varun-MSI <tvarun2014@gmail.com>
2024-06-01 00:49:24 +0530

    New

--- Scheduling Algorithms Tesseract/5_Priority_Preemptive.c
+++ Scheduling Algorithms Tesseract/5_Priority_Preemptive.c
@@ -15,52 +15,51 @@ Process Id	Priority	Arrival Time	Burst Time
 #include<stdio.h>
 
 int main(){
-    int processes = 7;
+    int n = 7;
     int processNames[] = {1,2,3,4,5,6,7};
     int priority[] = {2,6,3,5,4,10,9};
     int arrivalTime[] = {0,1,2,3,4,5,15};
     int burstTime[] = {1,7,3,6,5,15,8};
-    int remainingBurst[] = {1,7,3,6,5,15,8};
-    int completionTime[processes],TAT[processes],WT[processes];
+    int remaining[] = {1,7,3,6,5,15,8};
+    int completionTime[n],TAT[n],WT[n];
     float totalTat = 0, totalWt = 0;
-    int currentTime = 0;
-    int completed = 0;
+    int currTime = 0,completed = 0;
     
-    while(completed < processes){
-        int highestPriority = -1;
-        int highestPrIndex = -1;
-        for(int i = 0;i<processes;i++){
-            if(arrivalTime[i] <= currentTime && remainingBurst[i]>0 && priority[i]>highestPriority){
-                highestPriority = priority[i];
-                highestPrIndex = i;
+    while(completed < n){
+        int hPriority = -1;
+        int index = -1;
+        for(int i = 0;i<n;i++){
+            if(arrivalTime[i] <= currTime && remaining[i]>0 && priority[i]>hPriority){
+                hPriority = priority[i];
+                index = i;
             }
         }
-        if(highestPrIndex == -1){
-            currentTime++;
+        if(index == -1){
+            currTime++;
             continue;
         }
         
-        remainingBurst[highestPrIndex]--;
-        currentTime++;
+        remaining[index]--;
+        currTime++;
         
-        if(remainingBurst[highestPrIndex] == 0){
-            completionTime[highestPrIndex] = currentTime;
-            TAT[highestPrIndex] = completionTime[highestPrIndex] - arrivalTime[highestPrIndex];
-            WT[highestPrIndex] = TAT[highestPrIndex] - burstTime[highestPrIndex];
+        if(remaining[index] == 0){
+            completionTime[index] = currTime;
+            TAT[index] = completionTime[index] - arrivalTime[index];
+            WT[index] = TAT[index] - burstTime[index];
             
-            totalTat += TAT[highestPrIndex];
-            totalWt += WT[highestPrIndex];
+            totalTat += TAT[index];
+            totalWt += WT[index];
             
             completed++;
         }
     }
     
-    printf("Process\tPriority\tBurst Time\tArrival Time\tCompletion Time\tTurnaround Time\tWaiting Time\n");
-    for(int i = 0;i<processes;i++){
-        printf("%d\t%d\t\t%d\t\t%d\t\t%d\t\t%d\t\t\t%d\n",processNames[i],priority[i],burstTime[i],arrivalTime[i],completionTime[i],TAT[i],WT[i]);
+    printf("Process\tPriority\tBT\tAT\tCT\tTAT\tWT\n");
+    for(int i = 0;i<n;i++){
+        printf("%d\t%d\t\t%d\t%d\t%d\t%d\t%d\n",processNames[i],priority[i],burstTime[i],arrivalTime[i],completionTime[i],TAT[i],WT[i]);
     }
     
-    printf("Average Turn Around Time : %.2f\n",totalTat/processes);
-    printf("Average Waiting Time : %.2f\n",totalWt/processes);
+    printf("Average Turn Around Time : %.2f mins\n",totalTat/n);
+    printf("Average Waiting Time : %.2f mins\n",totalWt/n);
     return 0;
 }
