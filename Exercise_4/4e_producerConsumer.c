fa8d1a5c3ae68ee129730c3fd1bb513427087b88
Varun-MSI <tvarun2014@gmail.com>
2024-07-12 11:19:11 +0530

    New

--- Exercise_4/4e_producerConsumer.c
+++ Exercise_4/4e_producerConsumer.c
@@ -5,52 +5,43 @@
 #include <semaphore.h>
 #include <fcntl.h>
 
-
 #define SEM_PRODUCER "/semProducer"
 #define SEM_CONSUMER "/semConsumer"
 int main()
 {
-        sem_t *semProducer, *semConsumer;
-
-    // Initialize semaphores
-    semProducer = sem_open(SEM_PRODUCER, O_CREAT, 0644, 1); 
-    semConsumer = sem_open(SEM_CONSUMER, O_CREAT, 0644, 0); 
-      if (semProducer == SEM_FAILED || semConsumer == SEM_FAILED) {
+    sem_t *semProducer, *semConsumer;
+    semProducer = sem_open(SEM_PRODUCER, O_CREAT, 0644, 1);
+    semConsumer = sem_open(SEM_CONSUMER, O_CREAT, 0644, 0);
+    if (semProducer == SEM_FAILED || semConsumer == SEM_FAILED)
+    {
         perror("sem_open failed");
         exit(EXIT_FAILURE);
     }
     pid_t p;
- 
     p = fork();
-    if (p > 0) // parent
-    {   
+    if (p > 0)
+    {
         for (int i = 0; i < 5; i++)
-        { 
-              
-            
+        {
             sem_wait(semProducer);
             printf("Producing Item\n");
             sem_post(semConsumer);
             sleep(2);
-
         }
-            wait(NULL);
+        wait(NULL);
         sem_close(semProducer);
         sem_close(semConsumer);
         sem_unlink(SEM_PRODUCER);
         sem_unlink(SEM_CONSUMER);
-        
     }
-    else // child
-    {   
+    else
+    {
         for (int i = 0; i < 5; i++)
-        {   
+        {
             sem_wait(semConsumer);
             printf("Consuming item\n");
             sem_post(semProducer);
             sleep(2);
-          
         }
-        
     }
 }
