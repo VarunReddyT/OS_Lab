86d8c5a169a532e7bbbb3ea345da6237a09ccf52
Varun-MSI <tvarun2014@gmail.com>
2024-07-15 21:55:09 +0530

    New

--- Exercise_7/File_Info_2.c
+++ Exercise_7/File_Info_2.c
@@ -7,22 +7,22 @@ int main(int argc, char *argv[])
     struct stat a;
     for (i = 1; i < argc; i++)
     {
-        printf("\n%s", argv[i]);
+        printf("%s ", argv[i]);
         stat(argv[i], &a);
         if (S_ISDIR(a.st_mode))
         {
-            printf("Is a directory\n");
+            printf("is a directory\n");
         }
         else
         {
-            printf("Is a regular file\n");
+            printf("is a regular file\n");
         }
-        printf("\n FILE PROPERTIES \n");
+        printf("FILE PROPERTIES \n");
         printf("Inode number:%d\n", a.st_ino);
         printf("No of links:%d\n", a.st_nlink);
         printf("Last access time:%s", asctime(localtime(&a.st_atime)));
-        printf("permission Flag:%o\n", a.st_mode % 512);
-        printf("size:%d\n", a.st_size);
+        printf("Permission Flag:%o\n", a.st_mode % 512);
+        printf("Size:%d\n", a.st_size);
     }
     return 0;
 }
